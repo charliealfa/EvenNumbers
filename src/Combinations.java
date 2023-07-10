eb8dd1ee6ad91b928b97d04f63c12fe5400eaeb8
Cihan Aslan <cihan.aslan.qa@gmail.com>
2023-07-10 12:40:54 +0300

    Combination class created

---
+++ src/Combinations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Combinations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter main number : ");
+        int mainNum = scan.nextInt();
+        System.out.println("Please enter combination number : ");
+        int combinationNum = scan.nextInt();
+
+        System.out.println("combination of mainNum to combinationNum = " + combination(mainNum, combinationNum));
+
+    }
+
+    public static int factorial(int x) {
+        int result = 1;
+        while(x>0){
+            result*=x;
+            x--;
+        }
+        return result;
+    }
+
+    public static int combination(int x, int y) {
+        return factorial(x) / (factorial(y) * factorial((x-y)));
+    }
+}
